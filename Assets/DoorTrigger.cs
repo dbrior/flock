2cb718a7eeaa63363a054ebfe51500c9b059121c
Daniel Brior <djbrior@gmail.com>
2024-12-14 15:45:30 -0800

    Add gate open and close sound

--- Assets/DoorTrigger.cs
+++ Assets/DoorTrigger.cs
@@ -6,6 +6,9 @@ public class DoorTrigger : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private LayerMask openers;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip openSound;
+    [SerializeField] private AudioClip closeSound;
     private int numObjectsInRange;
 
     void OnTriggerEnter2D(Collider2D col) {
@@ -14,6 +17,10 @@ public class DoorTrigger : MonoBehaviour
 
         numObjectsInRange += 1;
         if (numObjectsInRange == 1) animator.SetTrigger("Open");
+
+        if (audioSource != null && openSound != null) {
+            audioSource.PlayOneShot(openSound);
+        }
     }
 
     void OnTriggerExit2D(Collider2D col) {
@@ -22,5 +29,9 @@ public class DoorTrigger : MonoBehaviour
 
         numObjectsInRange -= 1;
         if (numObjectsInRange == 0) animator.SetTrigger("Close");
+
+        if (audioSource != null && closeSound != null) {
+            audioSource.PlayOneShot(closeSound);
+        }
     }
 }
